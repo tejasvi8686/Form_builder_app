a555438c4c0df9e69d83abe04e66460407a33adf
tejavi8686 <tejasviraj8686@gmail.com>
2024-05-02 23:06:09 +0530

    create form and form card viewer

--- actions/form.ts
+++ actions/form.ts
@@ -2,7 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
-
+import { formSchema, formSchemaType } from "@/schemas/form";
 
 class UserNotFoundErr extends Error {}
 
@@ -40,4 +40,48 @@ export async function GetFormStats() {
     submissionRate,
     bounceRate,
   };
+}
+
+export async function CreateForm(data: formSchemaType) {
+  const validation = formSchema.safeParse(data);
+  if (!validation.success) {
+    throw new Error("form not valid");
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  const { name, description } = data;
+
+  const form = await prisma.form.create({
+    data: {
+      userId: user.id,
+      name,
+      description,
+    },
+  });
+
+  if (!form) {
+    throw new Error("something went wrong");
+  }
+
+  return form.id;
+}
+
+export async function GetForms() {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 }
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,19 +1,29 @@
-import { GetFormStats } from "@/actions/form";
+import { GetForms, GetFormStats } from "@/actions/form";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { ReactNode, Suspense } from "react";
 import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import { TbArrowBounce } from "react-icons/tb";
+import { Separator } from "@/components/ui/separator";
+import CreateFormBtn from "@/components/CreateFormBtn";
 
-export default function Home () {
+export default function Home() {
   return (
     <div className="container pt-4">
       <Suspense fallback={<StatsCards loading={true} />}>
         <CardStatsWrapper />
       </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
+      <Separator className="my-6" />
+      <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <CreateFormBtn />
+      </div>
     </div>
   );
-};
-
+}
 
 async function CardStatsWrapper() {
   const stats = await GetFormStats();
@@ -29,7 +39,7 @@ function StatsCards(props: StatsCardProps) {
   const { data, loading } = props;
 
   return (
-    <div className="w-full pt-8 grid grid-col-1 md:grid-col-2 lg:grid-col-4">
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       <StatsCard
         title="Total visits"
         icon={<LuView className="text-blue-600" />}
@@ -38,7 +48,32 @@ function StatsCards(props: StatsCardProps) {
         loading={loading}
         className="shadow-md shadow-blue-600"
       />
-     
+      <StatsCard
+        title="Total submissions"
+        icon={<FaWpforms className="text-yellow-600" />}
+        helperText="All time form submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-yellow-600"
+      />
+
+      <StatsCard
+        title="Submission rate"
+        icon={<HiCursorClick className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-green-600"
+      />
+
+      <StatsCard
+        title="Bounce rate"
+        icon={<TbArrowBounce className="text-red-600" />}
+        helperText="Visits that leaves without interacting"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-red-600"
+      />
     </div>
   );
 }
@@ -78,5 +113,20 @@ export function StatsCard({
         <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
       </CardContent>
     </Card>
+  );
+}
+
+function FormCardSkeleton() {
+  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
+}
+
+async function FormCards() {
+  const forms = await GetForms();
+  return (
+    <>
+      {forms.map((form) => (
+        <FormCard key={form.id} form={form} />
+      ))}
+    </>
   );
 }
