3e533af29e0d8cfec80a27fa5667ef7b156382df
tejavi8686 <tejasviraj8686@gmail.com>
2024-05-06 21:28:31 +0530

    form preview dialog

--- components/PreviewDialogBtn.tsx
+++ components/PreviewDialogBtn.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { MdPreview } from "react-icons/md";
-// import useDesigner from "./hooks/useDesigner";
+import useDesigner from "./hooks/useDesigner";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { FormElements } from "./FormElements";
 
 function PreviewDialogBtn() {
-//   const { elements } = useDesigner();
+  const { elements } = useDesigner();
 
   return (
     <Dialog>
@@ -16,7 +16,7 @@ function PreviewDialogBtn() {
           Preview
         </Button>
       </DialogTrigger>
-      {/* <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
+      <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
         <div className="px-4 py-2 border-b">
           <p className="text-lg font-bold text-muted-foreground">Form preview</p>
           <p className="text-sm text-muted-foreground">This is how your form will look like to your users.</p>
@@ -29,7 +29,7 @@ function PreviewDialogBtn() {
             })}
           </div>
         </div>
-      </DialogContent> */}
+      </DialogContent>
     </Dialog>
   );
 }
