92bd604733ee2728c1e4446bc66410be53561056
tejavi8686 <tejasviraj8686@gmail.com>
2024-05-06 18:37:09 +0530

    designer drap and drop

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import SidebarBtnElement from "./SidebarBtnElement";
 import { FormElements } from "./FormElements";
-// import useDesigner from "./hooks/useDesigner";
-// import FormElementsSidebar from "./FormElementsSidebar";
-// import PropertiesFormSidebar from "./PropertiesFormSidebar";
+import useDesigner from "./hooks/useDesigner";
+import FormElementsSidebar from "./FormElementsSidebar";
+import PropertiesFormSidebar from "./PropertiesFormSidebar";
 
 function DesignerSidebar() {
-//   const { selectedElement } = useDesigner();
+  const { selectedElement } = useDesigner();
   return (
     <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full">
         Elements
 
         <SidebarBtnElement formElement={FormElements.TextField} />
   
-      {/* {!selectedElement && <FormElementsSidebar />}
-      {selectedElement && <PropertiesFormSidebar />} */}
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </aside>
   );
 }
